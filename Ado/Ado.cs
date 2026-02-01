c7816f024b61caf4d50709805b3f2183f9bed931
Самойленко Денис Николаевич <samojlenko_d@itstep.academy>
2026-02-01 14:48:07 +0200

    ORM

--- Ado/Ado.cs
+++ Ado/Ado.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using DataManagementP47.Ado.Orm;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace DataManagementP47.Ado
 {
@@ -17,6 +19,9 @@ namespace DataManagementP47.Ado
             {
                 Console.WriteLine("1 - Перевірити підключення");
                 Console.WriteLine("2 - Створити таблиці БД");
+                Console.WriteLine("3 - Заповнити початкові дані");
+                Console.WriteLine("4 - Створити дані 1000 продажів");
+                Console.WriteLine("5 - Вивести дані про фірми");
                 Console.WriteLine("0 - Вихід");
                 keyInfo = Console.ReadKey();
                 Console.WriteLine();
@@ -25,12 +30,83 @@ namespace DataManagementP47.Ado
                 {
                     case '1': OpenConnection(); break;
                     case '2': CreateTables(); break;
+                    case '3': InsertData(); break;
+
+                    case '5': ShowFirms(); break;
                     case '0': break;
                     default: Console.WriteLine("Неправильний вибір"); break;
                 }
             } while (keyInfo.KeyChar != '0');
-            
+        }
 
+        private void ShowFirms()
+        {
+            if (connection is null)
+            {
+                Console.WriteLine("Підключення не встановлене, оберіть спочатку п.1");
+                return;
+            }
+            // Команди, що повертають результат. Вибірки
+            String sql = "SELECT * FROM Firms";
+            using SqlCommand cmd = new(sql, connection);
+            // Передачу резульатів забезпечує SqlDataReader
+            using SqlDataReader reader = cmd.ExecuteReader();
+            // "Гнучкість" до різної кількості колонок забезпечує індексація
+            // reader["Id"], reader["Name"]
+            // або спеціальні геттери
+            // reader.GetGuid("Id"), reader.GetString("Name")
+            // Геттери більш рекомендовані через типізацію
+            //    while (reader.Read())   // ітерація генератора - одержання одного рядка таблиці
+            //    {
+            //        Console.WriteLine("{0}  {1}", 
+            //            reader.GetGuid("Id"), 
+            //            reader.GetString("Name"));
+            //    }
+            // ORM - Object Relation Mapping - Відображення даних та їх зв'язків
+            //  на об'єкти та їх зв'язки. Використовується коли формат даних не
+            //  є сумісним з мовою програмування (БД, JSON, XML, Excel ...)
+            // У найпростішому випадку - це оголошення класів та утворення колекції
+            //  їх об'єктів
+            List<Firm> firms = [];
+            while (reader.Read())
+            {
+                firms.Add(Firm.FromReader(reader));   // Add(new Firm(reader))
+            }
+
+            // Від'єднаний режим -- працюємо з колекціями, попередньо завантаженими з БД
+            foreach (Firm firm in firms)
+            {
+                Console.WriteLine(firm);
+            }
+        }
+
+        private void InsertData()
+        {
+            /* Сідування (від англ. seed - зерно) - внесення початкових
+             * даних до системи, у т.ч. БД
+             */
+            if (connection is null)
+            {
+                Console.WriteLine("Підключення не встановлене, оберіть спочатку п.1");
+                return;
+            }
+            String sql = File.ReadAllText(
+                Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    "sql/InsertData.sql"
+            ));
+
+            using SqlCommand cmd = new(sql, connection);
+            try
+            {
+                cmd.ExecuteNonQuery();   // виконання без повернення результату
+                Console.WriteLine("Insert Data OK");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Insert Data fail: {ex.Message}");
+                Console.WriteLine(sql);
+            }
         }
 
         private void CreateTables()
@@ -105,4 +181,6 @@ Name  \-- FirmId   \--- EmployeeId  /   Name
           Birthday      Quantity   
                         Moment     
 
+Д.З. Наповнити БД "Академія" тестовими даними 
+Додати скріншоти з наповненими таблицями
  */
---
+++ Ado/Orm/Firm.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DataManagementP47.Ado.Orm
+{
+    internal class Firm
+    {
+        public Guid   Id   { get; set; }
+        public String Name { get; set; } = null!;
+
+        public static Firm FromReader(SqlDataReader reader)
+        {
+            return new Firm
+            {
+                Id = reader.GetGuid("Id"),
+                Name = reader.GetString("Name")
+            };
+        }
+
+        public override string ToString()
+        {
+            String first3 = Id.ToString()[..3];
+            String last3 = Id.ToString()[^3..];
+            return $"{first3}..{last3}  {Name}";
+        }
+    }
+}
